d0456e26885ec62d1e031741778383575dbd1e8b
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-29 03:29:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,24 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  namae = " "
+  nameholder = " "
+  data.each do |seasonal, arri|
+    if season == seasonal
+      arri.each do |individual_data|
+        individual_data.each do |data_type, value|
+          if data_type == "name"
+            nameholder = value
+            if data_type == "status"
+              if value == "Winner"
+                namae = nameholder
+              end 
+            end 
+          end 
+        end
+      end
+    end 
+  end
+  namae
 end
 
 def get_contestant_name(data, occupation)
