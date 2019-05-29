fd3662c7940d31170cfe57f86488bdda62398cde
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-29 03:37:53 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,7 @@ def get_first_name_of_season_winner(data, season)
   first_name = name_array.first
   first_name
 end
-
+#=================================================
 def get_contestant_name(data, occupation)
   # code here
 end
