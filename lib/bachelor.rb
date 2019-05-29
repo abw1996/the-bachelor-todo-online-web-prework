47d1c257920b39b4afbcae8001088810187c873a
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-29 03:59:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -68,7 +68,9 @@ def get_occupation(data, hometown)
           if value == hometown
             individual_data.each do |data_type1, value1|
               if data_type1 == "occupation"
-                occ = value1
+                if occ = " "
+                  occ = value1
+                end
               end 
             end 
           end 
